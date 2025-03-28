01b0ea95eb805035f60b532d9b7d2e705a521680
chiub2 <chiub2@mymacewan.ca>
2025-03-27 22:23:21 -0600

    hid api keys in .env

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDj10Tp-hLORAPl-O7shFRUS5CGNXUEHqo",
-  authDomain: "prepvue.firebaseapp.com",
-  projectId: "prepvue",
-  storageBucket: "prepvue.firebasestorage.app",
-  messagingSenderId: "1011670228016",
-  appId: "1:1011670228016:web:c365fec800d1ea7b6121f5",
-  measurementId: "G-1WL708YB7X"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
